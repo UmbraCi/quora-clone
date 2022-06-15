a81294e8b1549a647c0776e6b31a8ffa72b900c3
umbraci <umbraci@qq.com>
2022-06-15 17:50:45 +0800

    完成第一个页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -7,7 +8,8 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ColumnList, { ColumProps } from "./components/ColumnList.vue";
+import ColumnList, { ColumProps } from "@/components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "@/components/GlobalHeader.vue";
 
 const testData: ColumProps[] = [
   {
@@ -28,16 +30,28 @@ const testData: ColumProps[] = [
     avatar: "https://avatars0.githubusercontent.com/u/139426?s=460&v=4",
     description: "Vue.js是一个基于JavaScript的渐进式框架，由Mint-UI团队开发。",
   },
+  {
+    id: 4,
+    title: "Vue.js",
+    // avatar: "https://avatars0.githubusercontent.com/u/139426?s=460&v=4",
+    description: "Vue.js是一个基于JavaScript的渐进式框架，由Mint-UI团队开发。",
+  },
 ];
+const currentUser: UserProps = {
+  name: "UmbraCi",
+  isLogin: true,
+};
 
 export default defineComponent({
   name: "APP",
   components: {
     ColumnList,
+    GlobalHeader,
   },
   setup() {
     return {
       list: testData,
+      currentUser,
     };
   },
 });
---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="dropdown-option" :class="{ 'is-disable': disabled }">
+    <slot></slot>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "DropdownItem",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+<style lang="less">
+.dropdown-option.is-disable * {
+  color: #6c757d;
+  pointer-events: none;
+  background-color: transparent;
+}
+</style>
